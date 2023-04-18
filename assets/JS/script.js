95011f3e842cf986d0be494135bbd2ab0e14b313
Sammy <sammyhyi@gmail.com>
2023-04-18 10:42:32 -0400

    this is my html

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,1 @@
-var teamSearch = document.getElementById('teamSearch');
 
-// saves user input
-teamSearch.addEventListener('click', function() {
-  
-})
-
-// make function that gets Local data
-// make function that fetches data and displays
-// make function that determines winner
-// display the date of the game somewhere
