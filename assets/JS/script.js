ef50fb6d26a43dcef0e67eeb9b333f96aec6f4cd
Sammy <sammyhyi@gmail.com>
2023-04-18 19:55:35 -0400

    made function to grab the opponent id of team searched

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,6 +2,9 @@ var textAreaEl = document.getElementById('team-search');
 var searchBtn = document.getElementById('search-btn');
 var currentTeamEl = document.getElementById('user-team-name');
 
+
+
+
 // saves user input
 searchBtn.addEventListener('click', function(event) {
   event.preventDefault();
@@ -14,24 +17,56 @@ searchBtn.addEventListener('click', function(event) {
       'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
     }
   };
-  
-  fetch('https://api-nba-v1.p.rapidapi.com/teams', options)
+ 
+  fetch('https://api-nba-v1.p.rapidapi.com/teams?search=' + userInput, options)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      console.log(data.response[0]);
-      var currentTeam = data.response[0].name
-      console.log(currentTeam)
-      currentTeamEl.textContent = currentTeam
-      var teamID = data.response[0].id
-      console.log(teamID)
-      
+      console.log(data)
+      console.log(data.response[0].id)
+      var teamId = data.response[0].id
+      currentTeamEl.textContent = data.response[0].name
+      getOpp(teamId)
+
     })
     .catch(function(err) {
       console.error(err)
     });
 })
+
+
+
+function getOpp(teamId) {
+  console.log(teamId)
+  const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e2a9fb97bamshed18b6fd03679f7p164ac8jsn8814d63a32e0',
+        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+    }
+};
+
+fetch('https://api-nba-v1.p.rapidapi.com/games?league=standard&season=2022&team=' + teamId, options)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data)
+    var playOffGame = data.response.slice(-1);
+    var homeId = playOffGame[0].teams.home.id;
+    console.log(homeId);
+    var awayId = playOffGame[0].teams.visitors.id;
+    console.log(awayId);
+    var h2h = awayId + '-' + homeId;
+    console.log(h2h);
+    
+  })
+  .catch(function(err) {
+    console.error(err)
+  });
+}
+
 
 // make function that gets Local data
 // make function that fetches data and displays (make so only playoff teams show)
