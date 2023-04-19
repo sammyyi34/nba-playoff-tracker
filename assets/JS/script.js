ec7ef4196fdf36ebe42a3295f779dcb399ed010a
Sammy <sammyhyi@gmail.com>
2023-04-19 00:31:10 -0400

    got team stats to display and wrote comments

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,18 @@
 var textAreaEl = document.getElementById('team-search');
 var searchBtn = document.getElementById('search-btn');
 var currentTeamEl = document.getElementById('user-team-name');
+var oppTeamEl = document.getElementById('opp-team-name');
+var userStat1 = document.getElementById('user-stat1');
+var userStat2 = document.getElementById('user-stat2');
+var userStat3 = document.getElementById('user-stat3');
+var oppStat1 = document.getElementById('opp-stat1');
+var oppStat2 = document.getElementById('opp-stat2');
+var oppStat3 = document.getElementById('opp-stat3');
 
 
 
 
-// saves user input
+// saves user input then fetches that team
 searchBtn.addEventListener('click', function(event) {
   event.preventDefault();
   var userInput = textAreaEl.value;
@@ -23,8 +30,10 @@ searchBtn.addEventListener('click', function(event) {
       return response.json();
     })
     .then(function(data) {
+      // converts the user's searched team to corresponding team id
       var teamId = data.response[0].id
       currentTeamEl.textContent = data.response[0].name
+      //passes the value of teamID to the getOpp()
       getOpp(teamId)
 
     })
@@ -33,7 +42,7 @@ searchBtn.addEventListener('click', function(event) {
     });
 })
 
-
+// takes the searched team's id and finds their playoff matchup
 function getOpp(teamId) {
   const options = {
     method: 'GET',
@@ -48,20 +57,23 @@ function getOpp(teamId) {
     return response.json();
   })
   .then(function(data) {
+    // targets the lastest game played (would not work in dynamic setting)
     var playOffGame = data.response.slice(-1);
+    // distinguises searched team vs opponent by home/visiter id
     var homeId = playOffGame[0].teams.home.id;
     var awayId = playOffGame[0].teams.visitors.id;
+    // checks to see if searched team is the home or away by id and assigns the false to opponent
     var oppName = homeId === teamId ? awayId : homeId;
     if (homeId === teamId) {
       oppName = awayId;
     } else {
       oppName = homeId;
     }
-    console.log(oppName);
-    
-    
+    //passes both user team id and opponent team id to getStats()
+    getStats(teamId, oppName);
+    // for every matchup between the 2 teams
     var h2h = awayId + '-' + homeId;
-    getLastGames(h2h);
+    // getLastGames(h2h);
     
   })
   .catch(function(err) {
@@ -69,34 +81,42 @@ function getOpp(teamId) {
   });
 }
 
-function getLastGames(h2h) {
+// a fetch call that grabs team stats
+function getStats(teamId, oppName) {
+  console.log(teamId, oppName)
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '1796dbdd1dmsh6234b7a56ccb43bp136d3djsn7e79ec7f91d2',
+      'X-RapidAPI-Key': 'e2a9fb97bamshed18b6fd03679f7p164ac8jsn8814d63a32e0',
       'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
     }
   };
-
-  fetch('https://api-nba-v1.p.rapidapi.com/games?league=standard&season=2022&h2h=' + h2h, options)
-
+  
+  fetch('https://api-nba-v1.p.rapidapi.com/teams/statistics?id=' + teamId + '&season=2022', options)
   .then(function(response) {
     return response.json();
-  }) 
+  })
   .then(function(data) {
-    console.log(nextGame);
-    for (var i = 0; i < data.length; i++) {
-      console.log(data.length)
-    }
-
+    var ftp = data.response[0].fgp;
+    var tpp = data.response[0].tpp;
+    var points = data.response[0].points;
+    console.log(tpp)
+    // displays team stats to corresponding element
+    userStat1.innerHTML += ' ' + ftp + '%';
+    userStat2.innerHTML += ' ' + tpp + '%';
+    userStat3.innerHTML += ' ' + points + ' pts';
   })
   .catch(function(err) {
-    console.log(err)
-  })
+    console.error(err)
+  });
 }
+
+
+
 
 
 // make function that gets Local data
 // make function that fetches data and displays (make so only playoff teams show)
 // make function that determines winner
 // display the date of the game somewhere
+
